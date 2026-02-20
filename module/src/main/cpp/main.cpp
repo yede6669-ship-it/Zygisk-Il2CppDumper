8a184f5259771b6b975940267d5bbc4fb4a8a674
yeq <yede6669@gmail.com>
2026-02-20 23:49:54 +0800

    更新 main.cpp

--- module/src/main/cpp/main.cpp
+++ module/src/main/cpp/main.cpp
@@ -1,80 +1,64 @@
-#include <cstring>
-#include <thread>
+#include <jni.h>
+#include <string.h>
 #include <fcntl.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <unistd.h>
-#include <cinttypes>
-#include "hack.h"
+#include <sys/mman.h>
+#include <android/log.h>
 #include "zygisk.hpp"
-#include "game.h"
-#include "log.h"
+#include "dobby.h"
 
-using zygisk::Api;
-using zygisk::AppSpecializeArgs;
-using zygisk::ServerSpecializeArgs;
+#define LOG_TAG "DexUnpacker"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+
+// 备份原函数地址
+void (*old_OpenMemory)(uint8_t* base, size_t size, void* location, void* location_checksum, void* oat_dex_file, void* container, void* error_msg);
+
+// 脱壳回调函数
+void new_OpenMemory(uint8_t* base, size_t size, void* location, void* location_checksum, void* oat_dex_file, void* container, void* error_msg) {
+    if (base != nullptr && size > 0x30) {
+        // 检测 Dex 魔数
+        if (memcmp(base, "dex\n035", 7) == 0) {
+            char path[128];
+            // 导出到公用目录，文件名包含内存大小
+            sprintf(path, "/data/local/tmp/dump_%zu.dex", size);
+            int fd = open(path, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+            if (fd > 0) {
+                write(fd, base, size);
+                close(fd);
+                LOGI("Dump Success: %s, Size: %zu", path, size);
+            }
+        }
+    }
+    // 回调原函数
+    old_OpenMemory(base, size, location, location_checksum, oat_dex_file, container, error_msg);
+}
 
 class MyModule : public zygisk::ModuleBase {
 public:
-    void onLoad(Api *api, JNIEnv *env) override {
+    void onLoad(zygisk::Api* api, JNIEnv* env) override {
         this->api = api;
         this->env = env;
     }
 
-    void preAppSpecialize(AppSpecializeArgs *args) override {
-        auto package_name = env->GetStringUTFChars(args->nice_name, nullptr);
-        auto app_data_dir = env->GetStringUTFChars(args->app_data_dir, nullptr);
-        preSpecialize(package_name, app_data_dir);
-        env->ReleaseStringUTFChars(args->nice_name, package_name);
-        env->ReleaseStringUTFChars(args->app_data_dir, app_data_dir);
-    }
-
-    void postAppSpecialize(const AppSpecializeArgs *) override {
-        if (enable_hack) {
-            std::thread hack_thread(hack_prepare, game_data_dir, data, length);
-            hack_thread.detach();
+    void postAppSpecialize(const zygisk::AppSpecializeArgs*) override {
+        // Android 10-12 通用 OpenMemory 符号
+        const char* sym = "_ZN3art7DexFile10OpenMemoryEPKhjRKNSt3__112basic_stringIcNS3_11char_traitsIcEENS3_9allocatorIcEEEEjPNS_10MemMapPtrEPKNS_10OatDexFileEPS9_";
+        
+        // 使用 Dobby 查找符号地址
+        void* target = DobbySymbolResolver("libart.so", sym);
+        
+        if (target) {
+            // 执行 Hook
+            DobbyHook(target, (void*)new_OpenMemory, (void**)&old_OpenMemory);
+            LOGI("Dobby Hook Installed Successfully.");
+        } else {
+            LOGI("Symbol Not Found. Check your Android Version.");
         }
     }
 
 private:
-    Api *api;
-    JNIEnv *env;
-    bool enable_hack;
-    char *game_data_dir;
-    void *data;
-    size_t length;
-
-    void preSpecialize(const char *package_name, const char *app_data_dir) {
-        if (strcmp(package_name, GamePackageName) == 0) {
-            LOGI("detect game: %s", package_name);
-            enable_hack = true;
-            game_data_dir = new char[strlen(app_data_dir) + 1];
-            strcpy(game_data_dir, app_data_dir);
-
-#if defined(__i386__)
-            auto path = "zygisk/armeabi-v7a.so";
-#endif
-#if defined(__x86_64__)
-            auto path = "zygisk/arm64-v8a.so";
-#endif
-#if defined(__i386__) || defined(__x86_64__)
-            int dirfd = api->getModuleDir();
-            int fd = openat(dirfd, path, O_RDONLY);
-            if (fd != -1) {
-                struct stat sb{};
-                fstat(fd, &sb);
-                length = sb.st_size;
-                data = mmap(nullptr, length, PROT_READ, MAP_PRIVATE, fd, 0);
-                close(fd);
-            } else {
-                LOGW("Unable to open arm file");
-            }
-#endif
-        } else {
-            api->setOption(zygisk::Option::DLCLOSE_MODULE_LIBRARY);
-        }
-    }
+    zygisk::Api* api;
+    JNIEnv* env;
 };
 
 REGISTER_ZYGISK_MODULE(MyModule)
