4ab4ac40304f24904309c08960dadbcd37a7a4ea
yeq <yede6669@gmail.com>
2026-03-13 21:21:16 +0800

    更新 main.cpp

--- module/src/main/cpp/main.cpp
+++ module/src/main/cpp/main.cpp
@@ -1,39 +1,49 @@
-#include <jni.h>
-#include <android/log.h>
-#include <cstdio>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <thread>
 #include <string>
-#include "zygisk.hpp"
 #include "il2cpp_dump.h"
 
-#define LOG_TAG "IL2CPP_DUMPER"
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+// 监听线程逻辑
+void listen_for_api_commands() {
+    int server_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (server_fd < 0) return;
 
-extern "C" JNIEXPORT void JNICALL
-Java_com_unpacker_helper_DumpUtils_nativeTriggerDump(JNIEnv *env, jclass clazz, jstring save_path) {
-    if (save_path == nullptr) return;
+    struct sockaddr_un addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    
+    // 使用抽象命名空间（以 \0 开头），不需要担心文件权限
+    const char* socket_name = "\0il2cpp_dump_socket";
+    memcpy(addr.sun_path, socket_name, 19);
 
-    const char *path = env->GetStringUTFChars(save_path, nullptr);
-    if (path != nullptr) {
-        LOGI("收到指令：开始 IL2CPP Dump，保存至 %s", path);
-        
-        // 调用核心 il2cpp 提取逻辑
-        il2cpp_dump(path); 
-        
-        env->ReleaseStringUTFChars(save_path, path);
+    if (bind(server_fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+        close(server_fd);
+        return;
+    }
+
+    listen(server_fd, 1);
+
+    while (true) {
+        int client_fd = accept(server_fd, NULL, NULL);
+        if (client_fd > 0) {
+            char buffer[512] = {0};
+            // 读取辅助 App 发来的保存路径
+            read(client_fd, buffer, sizeof(buffer));
+            
+            LOGI("API 唤醒：开始执行 Dump 至 %s", buffer);
+            il2cpp_dump(buffer); // 调用核心脱壳逻辑
+            
+            close(client_fd);
+        }
     }
 }
 
 class MyModule : public zygisk::ModuleBase {
 public:
-    void onLoad(zygisk::Api* api, JNIEnv* env) override {
-        this->api = api;
-        this->env = env;
+    void postAppSpecialize(const zygisk::AppSpecializeArgs* args) override {
+        // 在游戏进程中启动监听线程
+        std::thread(listen_for_api_commands).detach();
     }
-
-private:
-    // 之前报错是因为少了这两行声明
-    zygisk::Api* api;
-    JNIEnv* env;
 };
-
-REGISTER_ZYGISK_MODULE(MyModule)
